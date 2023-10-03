93fd63586164bb0fa62a4a3120ee14f19d483339
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2023-10-03 17:08:48 +0530

    Add a --no-ansi option (#12)
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- src/ansible_creator/actions/init.py
+++ src/ansible_creator/actions/init.py
@@ -2,34 +2,44 @@
 
 from __future__ import annotations
 
-import logging
 import os
 import shutil
+
+from typing import TYPE_CHECKING
 
 from ansible_creator.exceptions import CreatorError
 from ansible_creator.templar import Templar
 from ansible_creator.utils import copy_container
 
 
-logger = logging.getLogger("ansible-creator")
+if TYPE_CHECKING:
+    from ansible_creator.output import Output
 
 
 class CreatorInit:
     """Class representing ansible-creator create subcommand."""
 
-    def __init__(self: CreatorInit, **kwargs: str) -> None:
+    def __init__(  # noqa: PLR0913
+        self: CreatorInit,
+        collection_name: str,
+        init_path: str,
+        force: bool,  # noqa: FBT001
+        creator_version: str,
+        output: Output,
+    ) -> None:
         """Initialize the init action.
 
         :param kwargs: Arguments passed for the init action
         """
-        self._namespace: str = kwargs["collection_name"].split(".")[0]
-        self._collection_name: str = kwargs["collection_name"].split(".")[-1]
+        self._namespace: str = collection_name.split(".")[0]
+        self._collection_name: str = collection_name.split(".")[-1]
         self._init_path: str = os.path.abspath(
-            os.path.expanduser(os.path.expandvars(kwargs["init_path"])),
+            os.path.expanduser(os.path.expandvars(init_path)),
         )
-        self._force = kwargs["force"]
-        self._creator_version = kwargs["creator_version"]
+        self._force = force
+        self._creator_version = creator_version
         self._templar = Templar()
+        self.output: Output = output
 
     def run(self: CreatorInit) -> None:
         """Start scaffolding collection skeleton.
@@ -38,7 +48,7 @@ class CreatorInit:
         """
         col_path = os.path.join(self._init_path, self._namespace, self._collection_name)
 
-        logger.debug("final collection path set to %s", col_path)
+        self.output.debug(msg="final collection path set to {col_path}")
 
         # check if init_path already exists
         if os.path.exists(col_path):
@@ -51,30 +61,30 @@ class CreatorInit:
             if not self._force:
                 msg = (
                     f"The directory {col_path} already exists.\n"
-                    f"{'':<9}You can use --force to re-initialize this directory,"
-                    f"\n{'':<9}However it will delete ALL existing contents in it."
+                    f"You can use --force to re-initialize this directory."
+                    f"\nHowever it will delete ALL existing contents in it."
                 )
                 raise CreatorError(msg)
 
             # user requested --force, re-initializing existing directory
-            logger.warning("re-initializing existing directory %s", col_path)
+            self.output.warning(f"re-initializing existing directory {col_path}")
             for root, dirs, files in os.walk(col_path, topdown=True):
                 for old_dir in dirs:
                     path = os.path.join(root, old_dir)
-                    logger.debug("removing tree %s", old_dir)
+                    self.output.debug(f"removing tree {old_dir}")
                     shutil.rmtree(path)
                 for old_file in files:
                     path = os.path.join(root, old_file)
-                    logger.debug("removing file %s", old_file)
+                    self.output.debug(f"removing file {old_file}")
                     os.unlink(path)
 
         # if init_path does not exist, create it
         if not os.path.exists(col_path):
-            logger.debug("creating new directory at %s", col_path)
+            self.output.debug(msg=f"creating new directory at {col_path}")
             os.makedirs(col_path)
 
         # copy new_collection container to destination, templating files when found
-        logger.debug("started copying collection skeleton to destination")
+        self.output.debug(msg="started copying collection skeleton to destination")
         copy_container(
             source="new_collection",
             dest=col_path,
@@ -84,11 +94,9 @@ class CreatorInit:
                 "collection_name": self._collection_name,
                 "creator_version": self._creator_version,
             },
+            output=self.output,
         )
 
-        logger.info(
-            "collection %s.%s successfully created at %s",
-            self._namespace,
-            self._collection_name,
-            self._init_path,
+        self.output.note(
+            f"collection {self._namespace}.{self._collection_name} created at {self._init_path}",
         )
--- src/ansible_creator/cli.py
+++ src/ansible_creator/cli.py
@@ -1,14 +1,17 @@
-"""The ansible-creator CLI."""
+"""The ansible-creator Cli."""
 
 from __future__ import annotations
 
 import argparse
-import logging
+import os
+import sys
 
 from importlib import import_module
+from pathlib import Path
 
 from ansible_creator.exceptions import CreatorError
-from ansible_creator.logger import ColoredFormatter, ExitOnExceptionHandler
+from ansible_creator.output import Output
+from ansible_creator.utils import TermFeatures
 
 
 try:
@@ -17,41 +20,82 @@ except ImportError:
     __version__ = "source"
 
 
-class AnsibleCreatorCLI:
-    """Class representing the ansible-creator CLI."""
+class Cli:
+    """Class representing the ansible-creator Cli."""
 
-    def __init__(self: AnsibleCreatorCLI) -> None:
-        """Initialize the CLI and parse CLI args."""
-        self.args: argparse.Namespace = self.parse_args()
-        self.logger: logging.Logger
+    def __init__(self: Cli) -> None:
+        """Initialize the Cli and parse Cli args."""
+        self.args: dict = vars(self.parse_args())
+        self.output: Output
+        self.term_features: TermFeatures
 
-    def init_logger(self: AnsibleCreatorCLI) -> None:
-        """Initialize the logger."""
-        self.logger = logging.getLogger("ansible-creator")
-        stream_handler = ExitOnExceptionHandler()
-        stream_handler.setLevel(logging.DEBUG)
-        colored_formatter = ColoredFormatter(
-            "%(levelname)s %(message)s",
-        )
-        stream_handler.setFormatter(colored_formatter)
-        self.logger.addHandler(stream_handler)
-        if self.args.verbose:
-            self.logger.setLevel(logging.DEBUG)
+    def init_output(self: Cli) -> None:
+        """Initialize the output object."""
+        no_ansi = self.args.pop("no_ansi")
+        if not sys.stdout.isatty():
+            self.term_features = TermFeatures(color=False, links=False)
         else:
-            self.logger.setLevel(logging.INFO)
+            self.term_features = TermFeatures(
+                color=False if os.environ.get("NO_COLOR") else not no_ansi,
+                links=not no_ansi,
+            )
 
-    def parse_args(self: AnsibleCreatorCLI) -> argparse.Namespace:
-        """Start parsing args passed from CLI.
+        self.output = Output(
+            log_append=self.args.pop("log_append"),
+            log_file=self.args.pop("log_file"),
+            log_level=self.args.pop("log_level"),
+            term_features=self.term_features,
+            verbosity=self.args.pop("verbose"),
+        )
 
-        :returns: A dictionary of CLI args.
+    def parse_args(self: Cli) -> argparse.Namespace:
+        """Start parsing args passed from Cli.
+
+        :returns: A dictionary of Cli args.
         """
         parent_parser = argparse.ArgumentParser(add_help=False)
 
         parent_parser.add_argument(
-            "--verbose",
-            help="Increase output verbosity",
+            "--na",
+            "--no-ansi",
             action="store_true",
-            required=False,
+            default=False,
+            dest="no_ansi",
+            help="Disable the use of ANSI codes for terminal color.",
+        )
+
+        parent_parser.add_argument(
+            "--lf",
+            "--log-file <file>",
+            dest="log_file",
+            default=str(Path.cwd() / "ansible-creator.log"),
+            help="Log file to write to.",
+        )
+
+        parent_parser.add_argument(
+            "--ll",
+            "--log-level <level>",
+            dest="log_level",
+            default="notset",
+            choices=["notset", "debug", "info", "warning", "error", "critical"],
+            help="Log level for file output.",
+        )
+
+        parent_parser.add_argument(
+            "--la",
+            "--log-append <bool>",
+            dest="log_append",
+            choices=["true", "false"],
+            default="true",
+            help="Append to log file.",
+        )
+
+        parent_parser.add_argument(
+            "-v",
+            "--verbose",
+            action="count",
+            default=0,
+            help="Give more Cli output. Option is additive, and can be used up to 3 times.",
         )
 
         parser = argparse.ArgumentParser(
@@ -67,7 +111,11 @@ class AnsibleCreatorCLI:
             help="Print ansible-creator version and exit.",
         )
 
-        subparsers = parser.add_subparsers(help="The command to invoke.", dest="action")
+        subparsers = parser.add_subparsers(
+            help="The subcommand to invoke.",
+            title="Commands",
+            dest="action",
+        )
         subparsers.required = True
 
         # 'init' command parser
@@ -100,30 +148,32 @@ class AnsibleCreatorCLI:
 
         return parser.parse_args()
 
-    def run(self: AnsibleCreatorCLI) -> None:
+    def run(self: Cli) -> None:
         """Dispatch work to correct action class."""
-        cli_args = vars(self.args)
-        self.logger.debug("parsed args %s", str(cli_args))
-        action = cli_args["action"]
+        # TO-DO: Convert CLI Args into a Config class
+        cli_args = self.args
+        self.output.debug(msg=f"parsed args {cli_args!s}")
+        action = cli_args.pop("action")
         action_modules = f"ansible_creator.actions.{action}"
         action_prefix = "Creator" + f"{action}".capitalize()
 
         try:
-            self.logger.info("starting requested action '%s'", action)
+            self.output.debug(msg=f"starting requested action '{action}'")
             action_class = getattr(import_module(action_modules), action_prefix)
-            self.logger.debug("found action class %s", action_class)
+            self.output.debug(f"found action class {action_class}")
+            cli_args.update({"output": self.output})
             cli_args.update({"creator_version": __version__})
             action_class(**cli_args).run()
         except CreatorError as exc:
-            self.logger.error(str(exc))
+            self.output.error(str(exc))
 
-        self.logger.debug("successfully exiting ansible-creator")
+        self.output.debug(msg="successfully exiting ansible-creator")
 
 
 def main() -> None:
-    """Entry point for ansible-creator CLI."""
-    cli = AnsibleCreatorCLI()
-    cli.init_logger()
+    """Entry point for ansible-creator Cli."""
+    cli = Cli()
+    cli.init_output()
     cli.run()
 
 
