ba83ba9976077a7ba63e353d42b9bf68fdb6e293
Kate Case <this.is@katherineca.se>
2024-04-02 09:51:59 -0400

    Fix existing init test for updated help

--- tests/integration/test_init.py
+++ tests/integration/test_init.py
@@ -13,43 +13,40 @@ def test_run_help(cli):
 
     # temporary assertion fix until we write custom helper
     if sys.version_info < (3, 10):
-        assert (
-            dedent(
-                """\
-                usage: ansible-creator [-h] [--version] {init} ...
+        text = dedent(
+            """\
+            usage: ansible-creator [-h] [--version] {init,docs} ...
 
-                Tool to scaffold Ansible Content. Get started by looking at the help text.
+            Tool to scaffold Ansible Content. Get started by looking at the help text.
 
-                optional arguments:
-                  -h, --help  show this help message and exit
-                  --version   Print ansible-creator version and exit.
+            optional arguments:
+              -h, --help   show this help message and exit
+              --version    Print ansible-creator version and exit.
 
-                Commands:
-                  {init}      The subcommand to invoke.
-                    init      Initialize an Ansible Collection.
-                """,
-            )
-            in result.stdout
+            Commands:
+              {init,docs}  The subcommand to invoke.
+                init       Initialize an Ansible Collection.
+                docs       (Re)generate documentation for an Ansible Collection.
+            """,
         )
     else:
-        assert (
-            dedent(
-                """\
-                usage: ansible-creator [-h] [--version] {init} ...
+        text = dedent(
+            """\
+            usage: ansible-creator [-h] [--version] {init,docs} ...
 
-                Tool to scaffold Ansible Content. Get started by looking at the help text.
+            Tool to scaffold Ansible Content. Get started by looking at the help text.
 
-                options:
-                  -h, --help  show this help message and exit
-                  --version   Print ansible-creator version and exit.
+            options:
+              -h, --help   show this help message and exit
+              --version    Print ansible-creator version and exit.
 
-                Commands:
-                  {init}      The subcommand to invoke.
-                    init      Initialize an Ansible Collection.
-                """,
-            )
-            in result.stdout
+            Commands:
+              {init,docs}  The subcommand to invoke.
+                init       Initialize an Ansible Collection.
+                docs       (Re)generate documentation for an Ansible Collection.
+            """,
         )
+    assert text in result.stdout
 
 
 def test_run_no_subcommand(cli):
