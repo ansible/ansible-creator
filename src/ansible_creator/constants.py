06ba914f24bb576571b99ca7a22cc439b8b43099
NilashishC <nilashishchakraborty8@gmail.com>
2024-04-22 11:07:56 +0530

    remove unused constants
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- src/ansible_creator/constants.py
+++ src/ansible_creator/constants.py
@@ -1,44 +1,5 @@
 """Definition of constants for this package."""
 
-MessageColors = {
-    "HEADER": "\033[94m",
-    "OKGREEN": "\033[92m",
-    "WARNING": "\033[93m",
-    "FAILURE": "\033[1;31m",
-    "OK": "\033[95m",
-    "ENDC": "\033[0m",
-}
-
-OPTION_CONDITIONALS = (
-    "mutually_exclusive",
-    "required_one_of",
-    "required_together",
-    "required_by",
-    "required_if",
-)
-
-OPTION_METADATA = (
-    "type",
-    "choices",
-    "default",
-    "required",
-    "aliases",
-    "elements",
-    "fallback",
-    "no_log",
-    "apply_defaults",
-    "deprecated_aliases",
-    "removed_in_version",
-)
-
-VALID_ANSIBLEMODULE_ARGS = (
-    "bypass_checks",
-    "no_log",
-    "add_file_common_args",
-    "supports_check_mode",
-    *OPTION_CONDITIONALS,
-)
-
 GLOBAL_TEMPLATE_VARS = {
     "DEV_CONTAINER_IMAGE": "ghcr.io/ansible/community-ansible-dev-tools:latest",
     "DEV_FILE_IMAGE": "ghcr.io/ansible/community-ansible-devspaces:latest",
