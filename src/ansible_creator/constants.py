10f253301ac68fcc524d56b7d9be9425008d12e0
Bradley A. Thornton <bthornto@redhat.com>
2024-05-02 14:55:35 -0700

    Add note

--- src/ansible_creator/constants.py
+++ src/ansible_creator/constants.py
@@ -7,4 +7,5 @@ GLOBAL_TEMPLATE_VARS = {
 
 # directory names that will be skipped in any resource
 SKIP_DIRS = ("__pycache__",)
+# file types that will be skipped in any resource
 SKIP_FILES_TYPES = (".pyc",)
