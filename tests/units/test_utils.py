57b7d14c87f03f322c3229bc29f7bdc57bbbc6ba
Bradley A. Thornton <bthornto@redhat.com>
2024-05-13 14:12:10 -0700

    Fix path

--- tests/units/test_utils.py
+++ tests/units/test_utils.py
@@ -7,9 +7,9 @@ from ansible_creator.utils import expand_path
 
 def test_expand_path() -> None:
     """Test expand_path utils."""
-    assert expand_path("~/$DEV_WORKSPACE/namespace/collection") == (
-        Path.home() / "/collections/ansible_collections/namespace/collection"
-    )
+    home = Path.home()
+    expected = home / "collections/ansible_collections/namespace/collection"
+    assert expand_path("~/$DEV_WORKSPACE/namespace/collection") == expected
     assert expand_path("~") == Path.home()
     assert expand_path("foo") == Path.cwd() / "foo"
     assert expand_path("$HOME") == Path.home()
