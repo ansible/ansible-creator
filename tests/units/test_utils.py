e45927b92432525e8b18e0ff9cc000d28b79b2e2
Bradley A. Thornton <bthornto@redhat.com>
2024-05-13 14:18:55 -0700

    Fix, resolve the home

--- tests/units/test_utils.py
+++ tests/units/test_utils.py
@@ -7,7 +7,7 @@ from ansible_creator.utils import expand_path
 
 def test_expand_path() -> None:
     """Test expand_path utils."""
-    home = Path.home()
+    home = Path.home().resolve()
     expected = home / "collections/ansible_collections/namespace/collection"
     assert expand_path("~/$DEV_WORKSPACE/namespace/collection") == expected
     assert expand_path("~") == Path.home()
