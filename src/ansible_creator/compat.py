004d2cb292ef835cf2e5249e95e9a29ed76f114c
Bradley A. Thornton <bthornto@redhat.com>
2024-05-14 14:51:58 -0700

    No ceverage for compat

--- src/ansible_creator/compat.py
+++ src/ansible_creator/compat.py
@@ -12,7 +12,7 @@ here.
 import sys
 
 
-if sys.version_info >= (3, 11):
+if sys.version_info >= (3, 11):  # pragma: no cover
     from importlib.resources.abc import Traversable as _Traversable
 else:
     from importlib.abc import (  # pylint: disable=deprecated-class
