d27c58310acbeb149f6e08c8436fa5baecaa9e8c
shatakshiiii <shatakshimishra01@gmail.com>
2024-06-21 13:29:12 +0530

    Cleanup in compat.py

--- src/ansible_creator/compat.py
+++ src/ansible_creator/compat.py
@@ -5,16 +5,12 @@ When we need to import a module differently depending on python versions, we do 
 here.
 """
 
-# ruff: noqa: F401
-
 import sys
 
 
 if sys.version_info >= (3, 11):
     from importlib.resources.abc import Traversable as _Traversable
 else:
-    from importlib.abc import (  # pylint: disable=deprecated-class
-        Traversable as _Traversable,
-    )
+    from importlib.abc import Traversable as _Traversable
 
 Traversable = _Traversable
