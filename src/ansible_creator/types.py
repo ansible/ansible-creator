60ef7df9d05a5371452d2c06b2ba1491dfd28134
Bradley A. Thornton <bthornto@redhat.com>
2024-06-25 09:15:18 -0700

    Update docstring

--- src/ansible_creator/types.py
+++ src/ansible_creator/types.py
@@ -14,12 +14,12 @@ class TemplateData:
         additions: A dictionary containing additional data to add to the gitignore.
         collection_name: The name of the collection.
         creator_version: The version of the creator.
-        namespace: The namespace of the collection.
-        scm_org: The organization of the source control management.
-        scm_project: The project of the source control management.
         dev_container_image: The devcontainer image.
         dev_file_image: The devfile image.
+        namespace: The namespace of the collection.
         recommended_extensions: A list of recommended VsCode extensions.
+        scm_org: The organization of the source control management.
+        scm_project: The project of the source control management.
     """
 
     additions: dict[str, dict[str, dict[str, str | bool]]] = field(default_factory=dict)
@@ -27,9 +27,9 @@ class TemplateData:
     creator_version: str = ""
     dev_container_image: Sequence[str] = GLOBAL_TEMPLATE_VARS["DEV_CONTAINER_IMAGE"]
     dev_file_image: Sequence[str] = GLOBAL_TEMPLATE_VARS["DEV_FILE_IMAGE"]
+    namespace: str = ""
     recommended_extensions: Sequence[str] = field(
         default_factory=lambda: GLOBAL_TEMPLATE_VARS["RECOMMENDED_EXTENSIONS"],
     )
-    namespace: str = ""
     scm_org: str = ""
     scm_project: str = ""
