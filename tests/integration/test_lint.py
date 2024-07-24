41ba3eb637e84485877a69f0fdfff0ac5b092228
Don Naro <dnaro@redhat.com>
2024-07-24 20:34:20 +0100

    more hacking

--- tests/integration/test_lint.py
+++ tests/integration/test_lint.py
@@ -1,9 +1,8 @@
 """Check scaffolded content integration with ansible-lint."""
 
 import re
-import shlex
 import subprocess
-
+import sys
 from pathlib import Path
 
 import pytest
@@ -15,6 +14,9 @@ def collection_path(tmp_path: Path) -> Path:
 
     Args:
         tmp_path: Temporary path.
+
+    Returns:
+        Path: Temporary directory for the collection.
     """
     test_path = ("collections", "ansible_collections")
     final_dest = tmp_path.joinpath(*test_path)
@@ -22,67 +24,72 @@ def collection_path(tmp_path: Path) -> Path:
     return final_dest
 
 
-def test_create_collection(collection_path: Path) -> None:
+@pytest.fixture()
+def scaffold_collection(collection_path: Path) -> Path:
     """Scaffold a collection with ansible-creator.
 
     Args:
         collection_path: Path for scaffolded collection.
+
+    Returns:
+        Path: Path for scaffolded collection.
     """
-    # Check that the collection path is present.
     assert collection_path.exists
 
-    # Define args for ansible-creator
-    creator_args = [
-        "ansible-creator",
+    creator_command = [
+        sys.executable,
+        "-Im",
+        "ansible_creator",
         "init",
         "testorg.testcol",
         "--init-path",
         str(collection_path),
     ]
 
-    # Join the args into a command
-    creator_command = shlex.join(creator_args)
+    result = subprocess.check_output(creator_command, text=True)
 
-    # Create a test collection in the testorg namespace.
+    assert re.search("Note: collection testorg.testcol created at", result) is not None
+
+    return collection_path
+
+
+def remove_ansi_escape_codes(text: str) -> str:
+    """Clean up ansi escape codes from stdout and stderr.
+
+    Args:
+        text: The input text string.
+
+    Returns:
+        str: The text string without ansi escape codes.
+    """
+    ansi_escape = re.compile(r"(?:\x1B[@-_][0-?]*[ -/]*[@-~])")
+    return ansi_escape.sub("", text)
+
+
+def test_lint_collection(scaffold_collection: Path) -> None:
+    """Lint the scaffolded collection with ansible-lint.
+
+    Args:
+        scaffold_collection: Path for scaffolded collection.
+    """
+    assert scaffold_collection.exists
+
     result = subprocess.run(
-        creator_command,
+        ["ansible-lint", scaffold_collection],
         text=True,
-        shell=True,
         capture_output=True,
         check=False,
     )
-    assert result.returncode == 0
 
-    # Check stdout for the collection creation message.
-    assert re.search("Note: collection testorg.testcol created at", result.stdout) is not None
-
-
-def test_lint_collection(collection_path: Path) -> None:
-    """Lint the scaffolded collection with ansible-lint."""
-    # Check that the scaffolded collection exists.
-    collection_dir = collection_path / "testorg" / "testcol"
-    assert collection_dir.exists
-
-    # Validate the collection with ansible-lint.
-    result = subprocess.run(
-        ["ansible-lint", collection_dir],
-        shell=True,
-        text=True,
-        capture_output=True,
-        check=False,
-    )
-
-    if result.returncode != 0:
-        print("Standard Output:", result.stdout)
-        print("Standard Error:", result.stderr)
+    clean_stdout = remove_ansi_escape_codes(result.stdout)
+    clean_stderr = remove_ansi_escape_codes(result.stderr)
 
     assert result.returncode == 0
 
-    # Check stdout for the linting success message.
     assert (
         re.search(
-            r"Passed: 0 failures, 0 warnings on \d+ files.*",
-            result.stdout,
+            r"Passed: 0 failure\(s\), 0 warning\(s\) on \d+ files.",
+            clean_stdout + clean_stderr,
         )
         is not None
     )
