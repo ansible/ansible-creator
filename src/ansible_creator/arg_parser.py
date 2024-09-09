b0c5328286fa59fef699371258f21a39742adc8b
Kate Case <this.is@katherineca.se>
2024-09-09 14:03:57 -0400

    Unsplit collection internally

--- src/ansible_creator/arg_parser.py
+++ src/ansible_creator/arg_parser.py
@@ -106,12 +106,6 @@ class Parser:
         # The internal still reference the old project name
         if self.args.project == "playbook":
             self.args.project = "ansible-project"
-            with contextlib.suppress(ValueError):
-                self.args.scm_org, self.args.scm_project = self.args.collection.split(
-                    ".",
-                    maxsplit=1,
-                )
-            self.args.collection = None
 
         return self.args, self.pending_logs
 
