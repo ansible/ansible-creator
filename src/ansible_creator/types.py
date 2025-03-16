ed332f682ee8741d86e5bc18f8f80c3c5aa62ffb
Shashank Venkat <shvenkat>
2025-03-16 19:18:38 +0530

    removed plugin_name

--- src/ansible_creator/types.py
+++ src/ansible_creator/types.py
@@ -34,7 +34,7 @@ class TemplateData:
 
     resource_type: str = ""
     plugin_type: str = ""
-    plugin_name: str = "{{ plugin_name }} | default('hello_world')"
+    plugin_name: str = ""
     additions: dict[str, dict[str, dict[str, str | bool]]] = field(default_factory=dict)
     collection_name: str = ""
     creator_version: str = ""
