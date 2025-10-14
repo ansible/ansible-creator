1b3a7450ea8c81bcd2b81fb3e2408cf6cbe8a4b3
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2025-10-14 13:49:48 +0100

    feat: make ansible creator show help options when arguments are not given (#486)

--- tests/defaults.py
+++ tests/defaults.py
@@ -2,9 +2,12 @@
 
 from __future__ import annotations
 
+import sys
+
 from pathlib import Path
 
 
 FIXTURES_DIR = (Path(__file__).parent / "fixtures").resolve()
+CREATOR_BIN = Path(sys.executable).parent / "ansible-creator"
 
 UUID_LENGTH = 8
---
+++ tests/integration/test_no_input.py
@@ -0,0 +1,47 @@
+"""Test running ansible-creator commands without input or --help."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from tests.defaults import CREATOR_BIN
+
+
+if TYPE_CHECKING:
+    from tests.conftest import CliRunCallable
+
+
+@pytest.mark.parametrize(
+    argnames=("args", "error_msg"),
+    argvalues=(
+        pytest.param("add", "Missing required argument 'content-type'.", id="add"),
+        pytest.param("add --help", "", id="add-help"),
+        pytest.param(
+            "add resource", "Missing required argument 'resource-type'.", id="add-resource"
+        ),
+        pytest.param("add resource --help", "", id="add-resource-help"),
+        pytest.param("add plugin", "Missing required argument 'plugin-type'.", id="add-plugin"),
+        pytest.param("add plugin --help", "", id="add-plugin-help"),
+        pytest.param("init", "Missing required argument 'project-type'.", id="init"),
+        pytest.param("init --help", "", id="init-help"),
+    ),
+)
+def test_run_no_input(cli: CliRunCallable, args: str, error_msg: str) -> None:
+    """Test running ansible-creator commands without input or --help.
+
+    Args:
+        cli: cli_run function.
+        args: args to pass to the CLI.
+        error_msg: Expected error message.
+
+    Raises:
+        AssertionError: If the assertion fails.
+    """
+    result = cli(f"{CREATOR_BIN} {args}", env={"NO_COLOR": "1"})
+    if not error_msg:
+        assert result.returncode == 0
+    else:
+        assert result.returncode != 0
+        assert error_msg in result.stderr
